e92f29a2f5003e34d1e12c6a86b4a3f539f5dc1a
nisarul-media <65971192+nisarul-media@users.noreply.github.com>
2021-03-13 02:09:55 +0600

    Making files reusable

---
+++ includes/db.php
@@ -0,0 +1,15 @@
+<?php
+$db['host'] = "localhost";
+$db['user'] = "root";
+$db['password']= "";
+$db['database'] = "cms";
+
+foreach ($db as $key => $value) {
+    define( strtoupper($key), $value );
+}
+
+$connection  = mysqli_connect(HOST, USER, PASSWORD, DATABASE);
+
+if ($connection) {
+    echo "Hello";
+}
