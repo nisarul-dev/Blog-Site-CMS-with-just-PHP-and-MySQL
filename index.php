ee7f4a2790aee64d26c8d05231b977a4147c8995
nisarul-media <65971192+nisarul-media@users.noreply.github.com>
2021-03-13 13:38:48 +0600

    post loop done

--- index.php
+++ index.php
@@ -17,21 +17,25 @@
             <small>Secondary Text</small>
         </h1>
 
-        <!-- First Blog Post -->
-        <h2>
-            <a href="#">Blog Post Title</a>
-        </h2>
-        <p class="lead">
-            by <a href="index.php">Start Bootstrap</a>
-        </p>
-        <p><span class="glyphicon glyphicon-time"></span> Posted on August 28, 2013 at 10:00 PM</p>
-        <hr>
-        <img class="img-responsive" src="http://placehold.it/900x300" alt="">
-        <hr>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, veritatis, tempora, necessitatibus inventore nisi quam quia repellat ut tempore laborum possimus eum dicta id animi corrupti debitis ipsum officiis rerum.</p>
-        <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+        <!-- Blog Post Loop -->
+        <?php
+        $posts_table = $connection->query("SELECT * FROM posts");
+        while($posts_table_obj = mysqli_fetch_object($posts_table)) :?>
+            <h2>
+                <a href="#"><?php echo $posts_table_obj->post_title; ?></a>
+            </h2>
+            <p class="lead">
+                by <a href="index.php"><?php echo $posts_table_obj->post_author; ?></a>
+            </p>
+            <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $posts_table_obj->post_date; ?></p>
+            <hr>
+            <img class="img-responsive" src="images/<?php echo $posts_table_obj->post_image; ?>" alt="">
+            <hr>
+            <p><?php echo $posts_table_obj->post_content; ?></p>
+            <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 
-        <hr>
+            <hr>
+        <?php endwhile; ?>
 
         <!-- Pager -->
 <!--        <ul class="pager">-->
